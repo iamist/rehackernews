dc5d775dca88d46500c6a95bf3722b34cb475ed1
iamist <vicentechristian@gmail.com>
2022-05-20 05:48:49 +0800

    adding story component

--- src/components/StoryCard.tsx
+++ src/components/StoryCard.tsx
@@ -10,11 +10,11 @@ const StoryCard: React.FC<StoryInterface> = (
     const {hostname} = url ? new URL(url) : {hostname: ''};
     return (
         <>
-            <a href={url ? url : '#'} className="text-sm text-black">{title} <span className="text-xs text-gray-600">{hostname ? `(${hostname})` : ''}</span></a>
+            <a href={url ? url : '#'} target="_blank" className="text-sm text-black">{title} <span className="text-xs text-gray-600">{hostname ? `(${hostname})` : ''}</span></a>
             <div className="text-xs text-gray-600 ml-4">
                 {score} points by {by} {moment(time).fromNow()} |
                 <Link className="ml-2" to={`https://news.ycombinator.com/hide?id=${id}&goto=${type}`}>hide</Link> |
-                <Link className="ml-2" to={`https://news.ycombinator.com/item?id=${id}`}>{descendants} comments</Link>
+                <Link className="ml-2" to={`/item/${id}`}>{descendants} comments</Link>
             </div>
         </>
     )
---
+++ src/components/StoryMeta.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import moment from 'moment';
+import StoryMetaInterface from '../services/types/StoryMetaInterface';
+
+const StoryMeta: React.FC<StoryMetaInterface> = ({id, title, score, by, time, descendants, type, showPassLink, showFavoriteLink}) => {
+    return (
+        <>
+            <div className="text-xs text-gray-600 ml-4">
+                <ul className='list-none flex flex-row'>
+                    <li className="mr-1 pr-1 border-gray-500 border-r">{score} points</li>
+                    <li className="mr-1 pr-1 border-gray-500 border-r">by {by}</li>
+                    <li className="mr-1 pr-1 border-gray-500 border-r">{moment(time).fromNow()}</li>
+                    <li className="mr-1 pr-1 border-gray-500 border-r"><a href={`https://news.ycombinator.com/hide?id=${id}&goto=${type}`}>hide</a></li>
+                    <li className="mr-1 pr-1 border-gray-500 border-r"><a href={`https://hn.algolia.com/?query=${title}&type=${type}&dateRange=all&sort=byDate&storyText=false&prefix&page=0`}>past</a></li>
+                    <li className="mr-1 pr-1 border-gray-500 border-r"><a href={`https://news.ycombinator.com/fave?id=${id}&auth=`}>favorite</a></li>
+                    <li>{descendants} comments</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default StoryMeta;
